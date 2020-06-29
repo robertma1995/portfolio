525116e3662a242c0e3062ba6a61978ae22ebd99
robertma1995 <r.ma@student.unsw.edu.au>
2020-06-29 21:10:06 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3c2c825c5fe83e987804a796ae801925.js"
+  "/portfolio/precache-manifest.1e6c04c7ed17f7c828ea3c4be19f1b37.js"
 );
 
 self.addEventListener('message', (event) => {
