ffcaefad83d2e151009b9cdd3ae8ad66a3a48ef9
robertma1995 <r.ma@student.unsw.edu.au>
2020-06-29 21:06:28 +1000

    added hotdog project details

--- src/utils/projectList.js
+++ src/utils/projectList.js
@@ -7,7 +7,7 @@ const projects = {
         demoUrl: "https://secret-hotdogs.web.app",
         codeUrl: "https://github.com/robertma1995/secret_hotdogs_react",
         technologies: ["React.js", "Firebase", "Material-UI", "PHP", "MySQL", "Express.js", "REST API"],
-        description: "This project started as a PHP (and MySQL) learning exercise. \n\n Later on, I decided I wanted more interactivity and real-time databases, hence React and Firebase. I also wanted to explore these two technologies beyond one university course. \n\n I used an Express REST API for accessing the database, but this couldn't be used for real-time data. The main challenge of this app was the conversion to direct Firebase calls instead of proxying through an API, and also learning React's newest way of state variable tracking (hooks).",
+        description: "This project started as a PHP (and MySQL) learning exercise. The goal was to create a social media-like web application where users could post, like, and comment on hotdog recipes. \n\n Later on, I wanted more interactivity and explore real-time databases, hence React and Firebase. I also wanted to explore these two technologies beyond one university course. \n\n Express REST API was initially used for accessing the database, but this couldn't be used for real-time data. The main challenge of this app was the conversion to direct Firebase calls instead of proxying through an API, and also learning React's newest way of state variable tracking (hooks). \n\n This is a work in progress - see GitHub link below for more details. \n\n Note: Sign Up function is disabled in current hosted version - sample login credentials can be provided on request",
     },
     "portfolio": {
         name: "Portfolio",
