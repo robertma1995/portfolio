29232b94eeb05f20f51f4914acb11720b4aebd5d
robertma1995 <r.ma@student.unsw.edu.au>
2021-03-07 15:13:02 +1100

    Updates

---
+++ precache-manifest.65aa737e0ba87fa91e499eb3a5c39f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f88bc909906c43bfbc260253abbb67",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "202fd5e151f80333a1a2",
+    "url": "/portfolio/static/css/main.39f797c2.chunk.css"
+  },
+  {
+    "revision": "cd909068f8b7a84c91c8",
+    "url": "/portfolio/static/js/2.a463808a.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/portfolio/static/js/2.a463808a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202fd5e151f80333a1a2",
+    "url": "/portfolio/static/js/main.7cd955bf.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7faea451ebeb1fbcb4e853c95e68da61.js"
+  "/portfolio/precache-manifest.65aa737e0ba87fa91e499eb3a5c39f4a.js"
 );
 
 self.addEventListener('message', (event) => {
