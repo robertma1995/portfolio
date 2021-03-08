933c10f26130e0733b2fe0a813d4ed2fd4ad53fa
robertma1995 <r.ma@student.unsw.edu.au>
2021-03-09 04:38:49 +1100

    updated video demo link

--- src/utils/projectList.js
+++ src/utils/projectList.js
@@ -5,7 +5,7 @@ const projects = {
         name: "Secret Ninja Hotdogs",
         logoUrl: "https://www.svgrepo.com/show/133687/hot-dog.svg",
         demoUrl: "https://secret-hotdogs.web.app",
-        videoUrl: "https://www.youtube.com/watch?v=UBHHEkR4ON0",
+        videoUrl: "https://www.youtube.com/watch?v=BGGiiBvF15M",
         codeUrl: "https://github.com/robert-jy-ma/secret_hotdogs_react",
         technologies: ["React.js", "Firebase", "Material-UI", "PHP", "MySQL", "Express.js", "REST API"],
         description: "Note: Sign Up function is disabled in demo at https://secret-hotdogs.web.app/ \nSample login credentials can be provided on request, or alternatively, watch the video demo. \n\n This project started as a PHP (and MySQL) learning exercise. The goal was to create a social media-like web application where users could post, like, and comment on hotdog recipes. \n\n Express REST API was initially used for accessing MySQL. Later on, I wanted more interactivity and explore real-time databases, hence React and Firebase. \n\n The main challenge of this app was combining real-time posting, editing, deleting and the pagination of posts. Currently, this is done by useEffect hooks and passing variables down child components. This avoids the overhead of using many snapshot listeners, and ensures there is no stale data.",
